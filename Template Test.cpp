db6008a7c746316e3b04cc699d5c9893a8ed08ea
Kipa5577 <72268856+Kipa5577@users.noreply.github.com>
2023-11-07 02:17:18 +0100

    Standardisation de la procédure de test/validation

---
+++ Template Test.cpp
@@ -0,0 +1,34 @@
+//dépendance 
+
+void main(void){
+
+
+
+
+//code de test 
+
+
+/* pour  test théorique 
+try{
+
+}
+
+try{
+
+
+}
+
+autant de try qu'il semble nécessaire pour montrer que la fonction se comporte normalement.
+
+*/
+
+
+}
+
+/***********************
+@Testeur: (ou Testeurs)
+@
+@Materiel: (matériel que concerne le test)
+@
+@date: (date du test qui a validé la fonction)
+************************/
